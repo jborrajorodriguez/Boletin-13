a85d74bf001c2298e35aabc1b9d530852b8408bf
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-12-04 10:05:18 +0100

    clase excepcion

---
+++ src/boletin/pkg13/TemperaturaErradaExcepcion.java
@@ -0,0 +1,28 @@
+package boletin.pkg13;
+
+import javax.swing.JOptionPane;
+
+/**
+ * @author Juan Borrajo Rodriguez Nº5937
+ */
+public class TemperaturaErradaExcepcion extends Exception{
+    private String mensaxe;
+    /**
+     * Constructor por defecto.
+     */
+    public TemperaturaErradaExcepcion() {
+        
+    }
+
+    public TemperaturaErradaExcepcion(String mensaxe) {
+        super(mensaxe);
+    }
+    
+    
+    
+    
+   
+        
+       
+
+}
